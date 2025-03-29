a2b6f6402de027d11fb0ba2a6ef7ce6b3fa96dcd
Vishwarenuka Boli <113835874+vboli@users.noreply.github.com>
2025-03-29 20:09:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import google.generativeai as genai
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
+## Function to load gemini model and get respones
 
 def get_gemini_response(input,image,prompt):
     model = genai.GenerativeModel('gemini-1.5-flash')
